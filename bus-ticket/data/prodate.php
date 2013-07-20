1fb9b6d0a7d402023648882e2b440dac078e6e4e
harinarayanan1994 <harinarayananroxx@gmail.com>
2013-07-20 16:46:43 +0530

    Update prodate.php

--- bus-ticket/data/prodate.php
+++ bus-ticket/data/prodate.php
@@ -38,8 +38,8 @@ if( $from==""||$to==""||$dep==""||$arr==""||$no==""||$rate=="")
 	die("Server Error:Please Fill all the required details");
 else if($from==$to||$dep==$arr)
 	die("Server Error:Invalid inputs");
-else if($_SESSION['log_user']=="")
-	{echo "<br><a href='http://localhost/bus-ticket/data/log.php'>LOGIN</a><br>";die("Please login to continue");
+else if(($_SESSION['log_user']!="hari_nara")||($_SESSION['pass']!="827ccb0eea8a706c4c34a16891f84e7b"))
+	{echo "<br><a href='http://localhost/bus-ticket/data/log.php'>LOGIN</a><br>";die("Please login as admin to continue");
 	}
 else 
 {
@@ -59,7 +59,7 @@ $result = mysql_query( $insert );
 if($result)
 {
 echo "<body bgcolor='wheat'><center><h1><b>".$_SESSION['log_user']."<br>Schedule Changed Successfully</b></h1><br>Login again  to continue..<br>
-       <a href='http://localhost/bus-ticket/data/log.php'>LOGIN</a></center></body>";
+       <a href='http://localhost/bus-ticket/data/out.php'>LOGOUT</a></center></body>";
 }
 else
       echo "activity failed.Please try again";
