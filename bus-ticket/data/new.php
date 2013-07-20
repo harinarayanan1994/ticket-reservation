7f6adb753173b192132e7ecf9e973760b47bae02
harinarayanan1994 <harinarayananroxx@gmail.com>
2013-07-20 16:44:42 +0530

    Update new.php

--- bus-ticket/data/new.php
+++ bus-ticket/data/new.php
@@ -35,8 +35,8 @@ if( $from==""||$to==""||$dep==""||$arr==""||$no==""||$rate=="")
 	die("Server Error:Please Fill all the required details");
 else if($from==$to||$dep==$arr)
 	die("Server error:Invalid input");
-else if($_SESSION['log_user']=="")
-	{echo "<br><a href='http://localhost/bus-ticket/data/log.php'>LOGIN</a><br>";die("Please login to continue");
+else if(($_SESSION['log_user']!="hari_nara")||($_SESSION['pass']!="827ccb0eea8a706c4c34a16891f84e7b"))
+	{echo "<br><a href='http://localhost/bus-ticket/data/log.php'>LOGIN</a><br>";die("Please login as admin to continue");
 	}
 
 else 
@@ -51,7 +51,7 @@ $result = mysql_query( $insert );
 if($result)
 {
 echo "<body bgcolor='wheat'><center><h1><b>".$_SESSION['log_user']."<br>NEW SERVICE ADDED SUCCESSFULLY</b></h1><br>Login again  to continue..<br>
-       		<a href='http://localhost/bus-ticket/data/log.php'>LOGIN</a></center></body>";
+       		<a href='http://localhost/bus-ticket/data/out.php'>LOGOUT</a></center></body>";
 }
 else
 	echo "Already Exists";
